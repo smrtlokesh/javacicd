f7b4177d82087f4626ade1afdc55f74564d67f6f
slokeshreddy7 <slokeshreddy7@dev.azure.com>
2022-12-05 18:28:32 +0530

    First

---
+++ src/main/java/com/example/javacicd/JavacicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.javacicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacicdApplication.class, args);
+	}
+
+}
